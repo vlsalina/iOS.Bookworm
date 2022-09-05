b186a2a43f68f87309999028a254f96c78c74693
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-09-04 21:18:54 -0700

    Created books with core data

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,75 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Vincent Salinas on 9/4/22.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var genre = ""
+    @State private var review = ""
+    @State private var rating = 3
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    TextEditor(text: $review)
+                    
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                } header: {
+                    Text("Write a review")
+                }
+                
+                Section {
+                    Button(action: {
+                        let newBook = Book(context: moc)
+                        
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.genre = genre
+                        newBook.review = review
+                        newBook.rating = Int16(rating)
+                        
+                        try? moc.save()
+                        dismiss()
+                        
+                    }) {
+                       Text("Save")
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
